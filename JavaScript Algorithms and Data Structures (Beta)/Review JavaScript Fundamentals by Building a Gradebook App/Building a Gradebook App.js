869e2885d5452ef74d55478a653b524f36ef2eb1
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 12:26:41 +0200

    Building a Gradebook App_step2

--- JavaScript Algorithms and Data Structures (Beta)/Review JavaScript Fundamentals by Building a Gradebook App/Building a Gradebook App.js
+++ JavaScript Algorithms and Data Structures (Beta)/Review JavaScript Fundamentals by Building a Gradebook App/Building a Gradebook App.js
@@ -17,4 +17,21 @@ function getAverage(scores) {
     return sum / scores.length;
   }
 
-  
+
+// step2
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
