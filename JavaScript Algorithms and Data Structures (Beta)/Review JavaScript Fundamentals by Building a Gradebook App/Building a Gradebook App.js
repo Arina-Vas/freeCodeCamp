c20f6919703f41678fe874f0b7b82ee29f7eb0b9
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 12:27:50 +0200

    Building a Gradebook App_step4

--- JavaScript Algorithms and Data Structures (Beta)/Review JavaScript Fundamentals by Building a Gradebook App/Building a Gradebook App.js
+++ JavaScript Algorithms and Data Structures (Beta)/Review JavaScript Fundamentals by Building a Gradebook App/Building a Gradebook App.js
@@ -41,3 +41,14 @@ function getGrade(score) {
 function hasPassingGrade(score) {
     return getGrade(score) !== "F";
 }
+
+// step4
+function studentMsg(totalScores, studentScore) {
+    if (hasPassingGrade(studentScore)) {
+        return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You passed the course."
+    }
+    else {
+        return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You failed the course."
+    }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
