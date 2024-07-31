42a5d1907cecc60dc4522f50084a3e43585926c5
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 12:33:15 +0200

    Building a Pyramid Generator_done

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/Building a Pyramid Generator.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/Building a Pyramid Generator.js
@@ -3,4 +3,27 @@
 
 // In this practice project, you'll learn fundamental programming concepts in JavaScript by coding your own Pyramid Generator. You'll learn how to work with arrays, strings, functions, loops, if/else statements, and more.
 
-// Solution
+// Solution:
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    } else {
+        rows.push(padRow(i, count));
+    }
+}
+
+let result = ""
+
+for (const row of rows) {
+    result = result + "\n" + row;
+}
+
+console.log(result);
